e75493c7ef53d48dfcafe4c5024250a6e68f9994
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-15 16:10:30 -0300

    Criação da Interface para Sala

---
+++ ReservaSalasDeAula/Reserva.Domain/Interfaces/ISalaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Reserva.Domain.Entidades;
+
+namespace Reserva.Domain.Interfaces
+{
+    public interface ISalaRepository
+    {
+        Task<IEnumerable<Sala>> GetSalasAsync();
+        Task<Sala> GetSalaByIdAsync(int? id);
+        Task<Sala> CreateAsync(Sala sala);
+        Task<Sala> UpdateAsync(Sala sala);
+        Task<Sala> RemoveAsync(Sala sala);
+    }
+}
