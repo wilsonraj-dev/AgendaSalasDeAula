34a8b61ad5026849bfd62cf842163df354485285
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-15 16:14:38 -0300

    Criação da Interface para Agenda

---
+++ ReservaSalasDeAula/Reserva.Domain/Interfaces/IAgendaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Reserva.Domain.Entidades;
+
+namespace Reserva.Domain.Interfaces
+{
+    public interface IAgendaRepository
+    {
+        Task<IEnumerable<Agenda>> GetAgendasAsync();
+        Task<IEnumerable<Agenda>> GetAgendasPorDataAsync(DateTime dataAgenda);
+        Task<IEnumerable<Agenda>> GetAgendasPorProfessorAsync(string professor);
+        Task<Agenda> CreateAsync(Agenda agenda);
+        Task<Agenda> UpdateAsync(Agenda agenda);
+        Task<Agenda> RemoveAsync(Agenda agenda);
+    }
+}
