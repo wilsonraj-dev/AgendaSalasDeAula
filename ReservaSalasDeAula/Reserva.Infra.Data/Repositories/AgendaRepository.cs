9acbeba221f34dbfa695ae9813afb48c5f666b19
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-15 21:46:08 -0300

    Implementação do repositório para Agenda

---
+++ ReservaSalasDeAula/Reserva.Infra.Data/Repositories/AgendaRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reserva.Domain.Entidades;
+using Reserva.Domain.Interfaces;
+using Reserva.Infra.Data.Context;
+
+namespace Reserva.Infra.Data.Repositories
+{
+    public class AgendaRepository : IAgendaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AgendaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Agenda>> GetAgendasAsync()
+        {
+            return await _context.Agendas.Where(x => x.DataAgenda >= DateTime.UtcNow).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Agenda>> GetAgendasPorDataAsync(DateTime dataAgenda)
+        {
+            return await _context.Agendas.TemporalBetween(dataAgenda, DateTime.UtcNow).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Agenda>> GetAgendasPorProfessorAsync(string professor)
+        {
+            return await _context.Agendas.Where(x => x.ProfessorResponsavel.Contains(professor)).ToListAsync();
+        }
+
+        public async Task<Agenda> CreateAsync(Agenda agenda)
+        {
+            _context.Add(agenda);
+            await _context.SaveChangesAsync();
+            return agenda;
+        }
+
+        public async Task<Agenda> UpdateAsync(Agenda agenda)
+        {
+            _context.Update(agenda);
+            await _context.SaveChangesAsync();
+            return agenda;
+        }
+
+        public async Task<Agenda> RemoveAsync(Agenda agenda)
+        {
+            _context.Remove(agenda);
+            await _context.SaveChangesAsync();
+            return agenda;
+        }
+    }
+}
