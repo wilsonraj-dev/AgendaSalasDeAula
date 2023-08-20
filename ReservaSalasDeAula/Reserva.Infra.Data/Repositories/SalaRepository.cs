26ff9bafed911e875f0d7b76051414fe6853f229
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-20 17:42:13 -0300

    Criação da repositório de Sala

---
+++ ReservaSalasDeAula/Reserva.Infra.Data/Repositories/SalaRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reserva.Domain.Entidades;
+using Reserva.Domain.Interfaces;
+using Reserva.Infra.Data.Context;
+
+namespace Reserva.Infra.Data.Repositories
+{
+    public class SalaRepository : ISalaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SalaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Sala>> GetSalasAsync()
+        {
+            return await _context.Salas.ToListAsync();
+        }
+
+        public async Task<Sala> GetSalaByIdAsync(int? id)
+        {
+            return await _context.Salas.FindAsync(id) ?? throw new ArgumentException();
+        }
+
+        public async Task<Sala> CreateAsync(Sala sala)
+        {
+            _context.Add(sala);
+            await _context.SaveChangesAsync();
+            return sala;
+        }
+
+        public async Task<Sala> UpdateAsync(Sala sala)
+        {
+            _context.Update(sala);
+            await _context.SaveChangesAsync();
+            return sala;
+        }
+
+        public async Task<Sala> RemoveAsync(Sala sala)
+        {
+            _context.Remove(sala);
+            await _context.SaveChangesAsync();
+            return sala;
+        }
+    }
+}
