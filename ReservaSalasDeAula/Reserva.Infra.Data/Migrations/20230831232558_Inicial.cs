3b5b90881f8e320b433bfd8de5d80b8edb6c7efb
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-31 20:27:35 -0300

    Criação da migration inicial

---
+++ ReservaSalasDeAula/Reserva.Infra.Data/Migrations/20230831232558_Inicial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reserva.Infra.Data.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Salas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeSala = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TipoSala = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agendas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataAgenda = table.Column<DateTime>(type: "Date", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ProfessorResponsavel = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    QtdeHorarios = table.Column<int>(type: "int", nullable: false),
+                    Disponivel = table.Column<bool>(type: "bit", nullable: false),
+                    SalaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agendas_Salas_SalaId",
+                        column: x => x.SalaId,
+                        principalTable: "Salas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendas_SalaId",
+                table: "Agendas",
+                column: "SalaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agendas");
+
+            migrationBuilder.DropTable(
+                name: "Salas");
+        }
+    }
+}
