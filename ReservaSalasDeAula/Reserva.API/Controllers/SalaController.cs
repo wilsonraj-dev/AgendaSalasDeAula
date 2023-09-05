b73470faa4c158ceeccbfd790445d48a025a04ae
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-09-05 20:50:56 -0300

    Criação da controller de Sala

---
+++ ReservaSalasDeAula/Reserva.API/Controllers/SalaController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reserva.Application.DTOs;
+using Reserva.Application.Interfaces;
+
+namespace Reserva.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SalaController : ControllerBase
+    {
+        private readonly ISalaService _service;
+
+        public SalaController(ISalaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<SalaDTO>>> GetSalasAsync()
+        {
+            var result = await _service.GetSalasAsync();
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<SalaDTO>> CreateAsync([FromBody] SalaDTO sala)
+        {
+            if (sala == null)
+            {
+                return BadRequest();
+            }
+
+            await _service.CreateAsync(sala);
+            return Ok(sala);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<SalaDTO>> UpdateAsync(int id, [FromBody] SalaDTO sala)
+        {
+            if (sala == null)
+            {
+                return BadRequest();
+            }
+            else if (sala.Id != id)
+            {
+                return BadRequest();
+            }
+
+            await _service.UpdateAsync(sala);
+            return Ok(sala);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<SalaDTO>> RemoveAsync(int id)
+        {
+            var result = await _service.GetSalaByIdAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            await _service.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
