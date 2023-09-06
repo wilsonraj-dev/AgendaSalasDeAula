ad9a576e81a6fddbd6d10e2da820a35da46bdcea
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-09-05 22:02:36 -0300

    Criação de migration para remoção da coluna Disponivel na tabela de Agenda

--- ReservaSalasDeAula/Reserva.Infra.Data/Migrations/20230906010105_Remocao_Coluna_Disponivel.Designer.cs
+++ ReservaSalasDeAula/Reserva.Infra.Data/Migrations/20230906010105_Remocao_Coluna_Disponivel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reserva.Infra.Data.Context;
 
@@ -11,9 +12,10 @@ using Reserva.Infra.Data.Context;
 namespace Reserva.Infra.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230906010105_Remocao_Coluna_Disponivel")]
+    partial class Remocao_Coluna_Disponivel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ReservaSalasDeAula/Reserva.Infra.Data/Migrations/20230906010105_Remocao_Coluna_Disponivel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reserva.Infra.Data.Migrations
+{
+    public partial class Remocao_Coluna_Disponivel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Disponivel",
+                table: "Agendas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Disponivel",
+                table: "Agendas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
